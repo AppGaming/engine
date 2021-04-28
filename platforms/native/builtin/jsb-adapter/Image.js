147cc808defcf6474d92752cf0bdd9260783692e
cuixianming <cuixianming@kuaishou.com>
2021-04-28 14:22:49 +0800

    添加直接使用 data 创建 Image 的构造函数

--- platforms/native/builtin/jsb-adapter/Image.js
+++ platforms/native/builtin/jsb-adapter/Image.js
@@ -1,8 +1,20 @@
 let HTMLImageElement = require('./HTMLImageElement');
 
 class Image extends HTMLImageElement {
-    constructor(width, height) {
+    constructor(data, width, height) {
+        if (!height) {
+            height = width;
+            width = data;
+            data = null;
+        }
         super(width, height, true)
+        
+        this._data = data;
+        this.complete = true;
+        if (data) {
+            var event = new Event('load');
+            this.dispatchEvent(event);
+        }
     }
 }
 
